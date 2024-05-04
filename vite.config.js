78348c930c7bcdc7ceaf12d95ba8c2f84574f38e
Нормуродов Шавкат <timanormu@gmail.com>
2024-05-04 18:30:50 +0500

    news

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+// vite.config.js
+import {
+    resolve
+ } from 'path'
+ import {
+    defineConfig
+ } from 'vite'
+ 
+ export default defineConfig({
+    build: {
+       rollupOptions: {
+          input: {
+             main: resolve(__dirname, 'index.html'),
+             train: resolve(__dirname, 'pages/train/index.html')
+          },
+       },
+    },
+ })
+
+ //location.assign('/pages/train/')
