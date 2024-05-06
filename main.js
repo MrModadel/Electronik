34c6a161d300977a26af6d837f2406207e981c54
Нормуродов Шавкат <timanormu@gmail.com>
2024-05-06 21:19:11 +0500

    news

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+let but = document.querySelector('.but-menu');
+let close = document.querySelector('.burgerMenu__close');
+let burgerMenu = document.querySelector('.burgerMenu');
+but.onclick = () => {
+    if (burgerMenu.classList[1] !== "active-br") {
+setTimeout(() => {
+    burgerMenu.style.bottom = '0';
+    
+}, 300);
+        burgerMenu.classList.add('active-br');
+
+    } else {
+        burgerMenu.classList.remove('active-br');
+        burgerMenu.style.bottom = '-100%';
+
+    }
+}
+close.onclick = () => {
+    burgerMenu.style.bottom = '-100%';
+
+    burgerMenu.classList.remove('active-br');
+
+}
